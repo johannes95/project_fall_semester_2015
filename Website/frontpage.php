330063a6791d810d99a44fc49d91db81779b8e10
soggan <gunnrup@gmail.com>
2015-12-09 09:07:39 +0100

    fixed a small bug

---
+++ Website/frontpage.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+        <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+        <link type="text/css" rel="stylesheet" href="css/frontpage.css" />
+        
+        <title> Celebrity Death# </title>
+        
+         <!--javascript files -->
+        <script type="text/javascript" src="js/jquery.js" charset="utf-8"></script> <!-- jquery library -->
+        <script type="text/javascript" src="js/Slider.js" charset="utf-8"></script>
+    </head>
+    <body>
+        <div id="wrapper">
+            <header>
+                <div id="logo">Celebrity Death#</div>
+                <a href="front_basic/login.php"><img src="img/sign_in/sign-in-with-twitter.png"/></a>
+            </header>
+            <div id="main">
+                <div id="slider">
+                    <div id="image1"></div>
+                    <div id="image2"></div>
+                </div>
+            </div>
+            <footer>&copy;Copyright MiB 2015</footer>
+        </div>
+    </body>
+</html>
+        
